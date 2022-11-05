b89e5955b6fb5854915d3212e2810492cf9cc561
feranmi@2002 <oluwaoyehunferanmi@gmail.com>
2022-11-05 01:53:14 +0100

    Removed import error

--- app/src/main/java/com/faithdeveloper/resume/ui/IntroFragment.kt
+++ app/src/main/java/com/faithdeveloper/resume/ui/IntroFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import com.faithdeveloper.resume.R
 import com.faithdeveloper.resume.Utils.GITHUB
 import com.faithdeveloper.resume.Utils.LINKEDIN
